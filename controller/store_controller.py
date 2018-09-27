c6b839175114902768dd50c09c739b7e00fe1313
Lucy <px.kula@gmail.com>
2018-09-27 22:11:27 +0200

    Revert "first running" i have to revert to version which working: This reverts commit f6db58c5b3ef0ee8cb599fc4852e9e4bd465e7af.

--- controller/store_controller.py
+++ controller/store_controller.py
@@ -18,8 +18,8 @@ def run():
     options = ["Add new record to table",
                "Remove a record with a given id from the table",
                "Updates specified record in the table",
-               "Question: How many different kinds of game are available of each manufacturer?",
-               "Question: What is the average amount of games in stock of a given manufacturer",
+               "How many different kinds of game are available of each manufacturer?",
+               "What is the average amount of games in stock of a given manufacturer",
                "back to main menu"]
 
     title_list = ["*id",
@@ -33,7 +33,6 @@ def run():
         terminal_view.print_menu("What do you want to do:",options,"Back to main menu")
         choice = terminal_view.get_choice(options)
         if choice == "1":
-
         #   to jest dzialajacy plik               model/store/games.csv
             file_name = common.get_input("Choose a file")
             table = common.get_table_from_file(file_name)
@@ -41,8 +40,6 @@ def run():
             record = terminal_view.get_inputs(title_list, "Enter data")
             table = store.add(table, record)
             common.write_table_to_file(file_name, table)
-
-
         elif choice == "2":
             file_name = get_input("Choose a file")
             table = common.get_table_from_file(file_name)
