3c6412259826c1f6a35e73ec0b0e90a42dcfcc6c
agaizuk <41573572+agaizuk@users.noreply.github.com>
2018-09-28 11:13:33 +0200

    Printing logo

--- view/terminal_view.py
+++ view/terminal_view.py
@@ -104,3 +104,20 @@ def get_inputs(list_labels, title):
 
 def print_error_message(message):
     print("\n" + message)
+    
+    
+def print_predator():
+    print ('''
+                            888        888                   
+                            888        888                   
+                            888        888                   
+88888b. 888d888 .d88b.  .d88888 8888b. 888888 .d88b. 888d888 
+888 "88b888P"  d8P  Y8bd88" 888    "88b888   d88""88b888P"   
+888  888888    88888888888  888.d888888888   888  888888     
+888 d88P888    Y8b.    Y88b 888888  888Y88b. Y88..88P888     
+88888P" 888     "Y8888  "Y88888"Y888888 "Y888 "Y88P" 888     
+888                                                          
+888                                                          
+888                                                          
+
+''')
